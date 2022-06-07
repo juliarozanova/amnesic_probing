e7e576af06ec213afd87df37a6afc5a38703b159
Julia Rozanova <juliarozanova@gmail.com>
2022-06-07 14:53:19 +0100

    further update path in runfile

--- runs/encode/run_encode.sh
+++ runs/encode/run_encode.sh
@@ -2,15 +2,14 @@ AMNESIC_PATH="./amnesic_probing/"
 cd $AMNESIC_PATH
 export PYTHONPATH=$AMNESIC_PATH
 
-split=$1
-input_file=$2
+split=$1 input_file=$2
 output_dir=$3
 task_format=$4
 encode_format=$5
 device=$6
 
 
-python amnesic_probing/encoders/encode.py \
+python amnesic_probing/amnesic_probing/encoders/encode.py \
         --input_file $input_file  \
         --output_dir $output_dir/$split/ \
         --encoder bert-base-uncased \
