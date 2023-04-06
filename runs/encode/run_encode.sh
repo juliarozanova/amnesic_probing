e88a738e4de7edafff0e8a569680e021354e56bf
Julia Rozanova <juliarozanova@gmail.com>
2023-04-06 14:47:06 +0100

    update forked for mnestic probing

--- runs/encode/run_encode.sh
+++ runs/encode/run_encode.sh
@@ -2,7 +2,8 @@ AMNESIC_PATH="PATH-TO-AMNESIC-PROBING"
 cd $AMNESIC_PATH
 export PYTHONPATH=$AMNESIC_PATH
 
-split=$1 input_file=$2
+split=$1 
+input_file=$2
 output_dir=$3
 task_format=$4
 encode_format=$5
